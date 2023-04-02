9e78b8b320f8529e455a10501aa48cf213cbfffe
Tatiana <Tatiana@users.noreply.github.com>
2023-04-02 10:22:32 +0000

    coment

--- test.py
+++ test.py
@@ -10,6 +10,11 @@ def prepare_db():
     yield
     db.drop_all()
 
+@pytest.fixture(autouse=True)
+def unlock_db():
+   yield
+   db.session.commit()
+
 
 def test_review_add():
     with app.test_client() as client:
@@ -20,6 +25,19 @@ def test_review_add():
         })
         assert response.status_code == 200
 
+def test_multiple_reviews():
+    with app.test_client() as client:
+        for i in range(3):
+            response = client.post('/add-review', json={
+                'book_name': 'FHJU',
+                'rating': 3,
+                'review': 'QWERTY'
+            })
+        response = client.get('/most-popular')
+        json = response.json
+        json_obj = json[0]
+        assert json_obj['review_count'] == 3
+
 def test_get_book_id():
     with app.test_client() as client:
         response = client.post('/search', json={
@@ -27,4 +45,3 @@ def test_get_book_id():
         })
         assert response.status_code == 200
         assert int(response.json['book_id']) == 1
-
